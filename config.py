6070968a35f03bfd4aeeb9b88f97146dfb11d26a
tfohrmann <till.fohrmann@posteo.de>
2025-12-04 17:27:03 +0100

    Include plotting setup and passive region extent in config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from matplotlib import colors
+
 FOCUS_REGION = {
     "x0": 2.5,  # longitude start
     "x1": 9.5,  # longitude end
@@ -6,3 +8,22 @@ FOCUS_REGION = {
     "y1": 53,   # latitude end
     "wy": 5     # latitude width
 }
+
+PASSIVE_REGION = {
+    "x0": -15,  # longitude start
+    "x1": 40,   # longitude end
+    "wx": 55,   # longitude width
+    "y0": 35,   # latitude start
+    "y1": 75,   # latitude end
+    "wy": 40    # latitude width  
+}
+
+# Plotting setups:
+SUM_LVL = [0, 0.1, 1 , 2, 5, 10, 15, 20, 30, 50, 75, 100, 125, 150, 200]    #precipitation sums
+SUM_NORM = colors.BoundaryNorm(SUM_LVL, 256)
+
+DIV_LVL = [-100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140, 160] #precipitation diffs
+DIV_NORM = colors.TwoSlopeNorm(vmin=-100, vcenter=0, vmax=160)
+
+PLOT_WINDOW = [-5, 25, 37, 62]      #Bounds: West, East, South, North
+
