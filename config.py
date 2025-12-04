85053ed111af0dea3c403a3c34db7e4ce20deebd
tfohrmann <till.fohrmann@posteo.de>
2025-12-04 14:31:40 +0100

    Start creating a configuration file

---
+++ config.py
@@ -0,0 +1,8 @@
+FOCUS_REGION = {
+    "x0": 2.5,  # longitude start
+    "x1": 9.5,  # longitude end
+    "wx": 7,    # longitude width
+    "y0": 48,   # latitude start
+    "y1": 53,   # latitude end
+    "wy": 5     # latitude width
+}
