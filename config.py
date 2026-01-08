97cd6c64e1fc07842041e694ae6f08ef4619fcc6
tfohrmann <till.fohrmann@posteo.de>
2026-01-08 15:01:48 +0100

    Correct passive_region extent and adjusting tracking color norm

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ FOCUS_REGION = {
 }
 
 PASSIVE_REGION = {
-    "x0": -15,  # longitude start
+    "x0": -10,  # longitude start
     "x1": 40,   # longitude end
-    "wx": 55,   # longitude width
-    "y0": 35,   # latitude start
+    "wx": 50,   # longitude width
+    "y0": 30,   # latitude start
     "y1": 75,   # latitude end
-    "wy": 40    # latitude width  
+    "wy": 45    # latitude width  
 }
 
 PRUDENCE_REGIONS = {"BI": {"name": "British Isles", "lon": slice(-10, 2), "lat": slice(50, 59)},
@@ -34,7 +34,7 @@ SUM_NORM = colors.BoundaryNorm(SUM_LVL, 256)
 DIV_LVL = [-100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140, 160] #precipitation diffs
 DIV_NORM = colors.TwoSlopeNorm(vmin=-100, vcenter=0, vmax=160)
 
-W2L_LVL = [0, 0.1, 0.5, 1., 1.5, 2., 3., 4.]
+W2L_LVL = [0, 0.1, 0.5, 1., 1.5, 2., 3., 4., 6.]
 W2L_NORM = colors.BoundaryNorm(W2L_LVL, 256)
 
 PLOT_WINDOW = [-5, 25, 37, 62]      #Bounds: West, East, South, North
