f2eeda6a5c4589dd26b1564f524814cda2897839
tfohrmann <till.fohrmann@posteo.de>
2026-04-27 13:57:37 +0200

    Update plotting colorbars

--- config.py
+++ config.py
@@ -9,6 +9,15 @@ FOCUS_REGION = {
     "wy": 5     # latitude width
 }
 
+FOCUS_REGION_L = {
+    "x0": 2,  # longitude start
+    "x1": 12,  # longitude end
+    "wx": 10,    # longitude width
+    "y0": 47,   # latitude start
+    "y1": 54,   # latitude end
+    "wy": 7     # latitude width
+}
+
 PASSIVE_REGION = {
     "x0": -10,  # longitude start
     "x1": 40,   # longitude end
@@ -31,16 +40,16 @@ PRUDENCE_REGIONS = {"BI": {"name": "British Isles", "lon": slice(-10, 2), "lat":
 SUM_LVL = [0, 0.1, 1 , 2, 5, 10, 15, 20, 30, 50, 75, 100, 125, 150, 200]    #precipitation sums
 SUM_NORM = colors.BoundaryNorm(SUM_LVL, 256)
 
-DIV_LVL = [-100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140, 160] #precipitation diffs
-DIV_NORM = colors.TwoSlopeNorm(vmin=-100, vcenter=0, vmax=160)
+DIV_LVL = [-160, -140, -120, -100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100, 120, 140, 160] #precipitation diffs
+DIV_NORM = colors.TwoSlopeNorm(vmin=-160, vcenter=0, vmax=160)
 
-W2L_LVL = [0, 0.1, 0.5, 1., 1.5, 2., 3., 4., 6.]
+W2L_LVL = [0, 0.1, 0.5, 1., 1.5, 2., 3., 4., 6.] #large:W2L_LVL = [0, 0.1, 0.5, 1., 1.5,  3., 5., 9.]
 W2L_NORM = colors.BoundaryNorm(W2L_LVL, 256)
 
 PLOT_WINDOW = [-5, 25, 37, 62]      #Bounds: West, East, South, North
 
 
-#Colors:
+# Colors:
 c_ctl = "tab:blue"
 c_dry = "tab:orange"
 c_wet = "tab:green"
